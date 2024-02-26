654861874b19dc25375387a445fab8606c17928b
Timóteo Santos <46605805+TimoteoSantos@users.noreply.github.com>
2024-02-26 18:38:50 -0300

    Add files via upload

---
+++ ALGORITMOS/CODIGOS/operadores relacionais .py
@@ -0,0 +1,21 @@
+#operadores relacionais
+
+# == retorna True caso antenda as duas condicoes
+a = 1
+b = 1
+c = 2
+
+if (a == b):
+    print ("as duas condicoes sao verdadeiras")
+    
+# != se os dois valores sao diferentes
+if (a != c):
+    print("as duas condicoes sao diferentes")
+    
+# > < >= <= sao condicoes para saber maior menor...
+
+if (a > c):
+    print ("a é maior que c")
+elif (a < c):
+    print ("a é menor que c")
+    
