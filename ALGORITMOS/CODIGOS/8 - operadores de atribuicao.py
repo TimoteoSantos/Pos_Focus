95e07eb176cb33ad684446b8dcdb5582911f0055
Timóteo Santos <46605805+TimoteoSantos@users.noreply.github.com>
2024-02-26 18:29:54 -0300

    Add files via upload

---
+++ ALGORITMOS/CODIGOS/8 - operadores de atribuicao.py
@@ -0,0 +1,58 @@
+#operadores de atribuicao
+# =, +=, -=, *=, /=, %=, **=, //=
+
+# = atribui o valor da direita na variavel da esquerda
+a = 10
+b = a
+print (b)
+
+# += soma o valor da direita com o valor da esquerda
+a = 10
+b = 2
+a += b
+print(a)
+
+
+# -= subtrai o valor da direita com o valor da esquerda
+a = 10
+b = 2
+a -= b
+print(a)
+
+# *= multiplica o valor da direita com o valor da esquerda
+a = 10
+b = 2
+a *= b
+print(a)
+
+# %= divide o valor da direita com o valor da esquerda e atribui o resto da divisao
+a = 10
+b = 2
+a %= b
+print(a)
+
+
+# **= Calcula o exponencial do valor da variá-
+#vel da esquerda elevado ao valor a direita,
+#e coloca o valor resultante na variável da
+#esquerda
+
+a = 3
+b = 3
+a **= b
+print(a)
+
+# //= divide o valor da variavel da direita pela esquerda e coloca a parte inteira
+# na variavel esquerda
+
+a = 3
+b = 2
+a //= b
+print (a)
+
+
+
+
+
+
+
